e7286dee8cb5aaa4ccf05bcc1b75d2547abfb5db
Zach <zacharysousa@outlook.com>
2025-11-06 15:15:41 -0500

    adjusted camera position and removed centering of tiles and added comments

--- scenes/procedural_generation.gd
+++ scenes/procedural_generation.gd
@@ -5,8 +5,8 @@ const TILE_SIZE = 16
 const WALL_SCENE: PackedScene = preload("res://scenes/test_wall.tscn")
 const FLOOR_SCENE: PackedScene = preload("res://scenes/test_floor.tscn")
 
-const MAP_WIDTH = 256
-const MAP_HEIGHT = 256
+const MAP_WIDTH = 100
+const MAP_HEIGHT = 100
 
 const NUM_GENERATIONS = 4
 # Alive represents walls, dead is floor.
@@ -19,7 +19,7 @@ var map = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	map = initialize_map(MAP_WIDTH, MAP_HEIGHT, INITIAL_ALIVE_PERCENT)
-	draw_map(TILE_SIZE, map)
+	draw_map(TILE_SIZE, map, WALL_SCENE, FLOOR_SCENE)
 	print("There are this many rows: " + str(map.size()))
 
 
@@ -27,6 +27,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
+	
+# Fills the map with random tiles reprenting either alive or dead. Tiles have a
+# 'chance' percent chance of being alive, otherwise they are dead.
 func initialize_map(width: int, height: int, chance: float) -> Array:
 	var array = []
 	for i in height:
@@ -39,16 +42,19 @@ func initialize_map(width: int, height: int, chance: float) -> Array:
 		array.append(row)
 	return array
 
-func draw_map(tile_scale: int, array: Array) -> void:
+# Draws the map by instantiating walls or floors based on if the cell is true (wall)
+# or false (floor). Each object is spawned at the position 
+# (x-index * tile_scale, y-index * tile_scale)
+func draw_map(tile_scale: int, array: Array, wall: PackedScene, floor: PackedScene) -> void:
 	for i in array.size() - 1:
 		for j in array[i].size() - 1:
 			if(array[i][j]):
-				var wall_instance = WALL_SCENE.instantiate()
+				var wall_instance = wall.instantiate()
 				wall_instance.position = Vector2(tile_scale * i, tile_scale * j)
 				add_child(wall_instance)
 				print("Created a wall at: " + str(wall_instance.position))
 			else:
-				var floor_instance = FLOOR_SCENE.instantiate()
+				var floor_instance = floor.instantiate()
 				floor_instance.position = Vector2(tile_scale * i, tile_scale * j)
 				add_child(floor_instance)
 				print("Created a floor at: " + str(floor_instance.position))
